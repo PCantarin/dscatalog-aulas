cf4736379ad776c3931715bc7e1eebd468d797b3
Pedro Cantarin <pedro.cantarin98@gmail.com>
2026-05-13 10:17:01 -0300

    Database seeding 'with import.sql'

--- backend/src/main/java/com/devsuperior/dscatalog/config/TestConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/TestConfig.java
@@ -1,30 +1,16 @@
 package com.devsuperior.dscatalog.config;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.repositories.CategoryRepository;
-
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
-
-	@Autowired
-	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Category c1 = new Category(null, "Eletronics");
-		Category c2 = new Category(null, "Computers");
-		Category c3 = new Category(null, "Video-Games");
-		
-		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
